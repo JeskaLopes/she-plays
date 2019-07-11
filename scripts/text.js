824b5f77ca44d8a12070fe7e429c94e9858bd6bb
JeskaLopes <jessica97cantora@hotmail.com>
2019-07-11 10:00:28 -0300

    adiciona campos no form

--- scripts/text.js
+++ scripts/text.js
@@ -2,12 +2,14 @@ let form = document.getElementById('form')
 let nome = document.getElementById('first');
 let sobrenome = document.getElementById('last')
 let campo = document.getElementById('exampleFormControlTextarea1');
+let nick = document.getElementById('nick');
+let hora = document.getElementById('hour');
 let sucesso = document.getElementById('divSucesso');
 let erro = document.getElementById('divErro');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault(); 
-    if(nome.value == "" | sobrenome.value =="" | campo.value == ""){
+    if(nome.value == "" | sobrenome.value =="" | campo.value == "" | nick.value == "" | hora.value ==""){
         alert("Preencha todos os campos"), $(erro).addClass('active')
     }
     else{
